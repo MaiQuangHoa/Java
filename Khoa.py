5ceede5a21573d3e37ba51154f888877a94ac6d8
0ngmai123@gmail.com <0ngmai123@gmail.com>
2018-04-23 16:23:20 +0700

    Xoa file .py

--- Khoa.py
+++
@@ -1,8 +0,0 @@
-class Khoa:
-	maKhoa =''
-	tenKhoa = ''
-	def __init__ (self, MaKhoa, TenKhoa):
-		self.maKhoa = MaKhoa
-		self.tenKhoa = TenKhoa
-	def Xuat(self):
-		print '%s  %s' %(self.maKhoa, self.tenKhoa)
